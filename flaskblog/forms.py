a44f871ef17aef00f17551c8d748c01ad430dfa9
Hasib Ahmad Khaliqi <hkhaliqi@netlinks.af>
2022-04-25 09:27:12 +0430

    initial commit

---
+++ flaskblog/forms.py
@@ -0,0 +1,87 @@
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
+from flask_login import current_user
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
+from wtforms.validators import DataRequired, Length, EqualTo, Email, ValidationError
+from flaskblog.models import User
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', 
+                            validators=[
+                                DataRequired(),
+                                Length(min=2, max=20)
+                            ])
+    email = StringField('Email', 
+                         validators=[
+                             DataRequired(),
+                             Email()
+                         ])
+    password = PasswordField('Password', 
+                              validators=[
+                                  DataRequired()
+                              ])
+    confirm_password = PasswordField('Confirm Password', 
+                                      validators=[
+                                          DataRequired(),
+                                          EqualTo('password')
+                                      ])
+    is_blogger = BooleanField('I am a blogger')
+    submit = SubmitField('Sign Up')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('username already take!')
+    
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('email already take!')
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', 
+                         validators=[
+                             DataRequired(),
+                             Email()
+                         ])
+    password = PasswordField('Password', 
+                              validators=[
+                                  DataRequired()
+                              ])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Login')
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username', 
+                            validators=[
+                                DataRequired(),
+                                Length(min=2, max=20)
+                            ])
+    email = StringField('Email', 
+                         validators=[
+                             DataRequired(),
+                             Email()
+                         ])
+    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
+    submit = SubmitField('Update')
+
+
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('username already take!')
+    
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('email already take!')
+
+class PostForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    description = TextAreaField('Description', validators=[DataRequired()])
+    content = TextAreaField('Content', validators=[DataRequired()])
+    picture = FileField('add a picture', validators=[FileAllowed(['jpg', 'png'])])
+    submit = SubmitField('Post')
---
+++ flaskblog/models.py
@@ -0,0 +1,61 @@
+from datetime import datetime
+from flaskblog import db, login_manager
+from flask_login import UserMixin
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    posts = db.relationship('Post', backref='author', lazy=True)
+    is_admin = db.Column(db.Boolean, nullable=False, default=False)
+    is_blogger = db.Column(db.Boolean, nullable=False, default=False)
+    
+    liked = db.relationship(
+        'PostLike',
+        foreign_keys='PostLike.user_id',
+        backref='user', lazy='dynamic')
+
+    def like_post(self, post):
+        if not self.has_liked_post(post):
+            like = PostLike(user_id=self.id, post_id=post.id)
+            db.session.add(like)
+
+    def unlike_post(self, post):
+        if self.has_liked_post(post):
+            PostLike.query.filter_by(
+                user_id=self.id,
+                post_id=post.id).delete()
+
+    def has_liked_post(self, post):
+        return PostLike.query.filter(
+            PostLike.user_id == self.id,
+            PostLike.post_id == post.id).count() > 0
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    is_approved = db.Column(db.Boolean, nullable=False, default=False)
+    likes = db.relationship('PostLike', backref='post', lazy='dynamic')
+
+    def __repr__(self):
+        return f"User('{self.title}', '{self.date_posted}')"
+
+class PostLike(db.Model):
+    # __tablename__ = 'post_like'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id'))
